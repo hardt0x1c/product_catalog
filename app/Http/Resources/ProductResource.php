1d56ed3227d790f6408318ad30ec7888c52f2ad6
Constantine <beloglazov.constantin@gmail.com>
2024-09-21 00:45:09 +0300

    feat: added product resource

---
+++ app/Http/Resources/ProductResource.php
@@ -0,0 +1,31 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Http\Resources;
+
+use App\Models\Product;
+use Illuminate\Http\Request;
+use Illuminate\Http\Resources\Json\JsonResource;
+
+/**
+ * @mixin Product
+ */
+final class ProductResource extends JsonResource
+{
+    /**
+     * Transform the resource into an array.
+     *
+     * @return array<string, mixed>
+     */
+    public function toArray(Request $request): array
+    {
+        return [
+            'id' => $this->getKey(),
+            'name' => $this->name,
+            'description' => $this->description,
+            'price' => $this->price,
+            'category' => new CategoryResource($this->whenLoaded('category')),
+        ];
+    }
+}
