b447c6e6012b5e4c681683ff77009eeda5b14d57
Constantine <beloglazov.constantin@gmail.com>
2024-09-21 00:44:54 +0300

    feat: added product requests

---
+++ app/Http/Requests/Product/UpdateProductRequest.php
@@ -0,0 +1,32 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Http\Requests\Product;
+
+use Illuminate\Foundation\Http\FormRequest;
+
+final class UpdateProductRequest extends FormRequest
+{
+    /**
+     * Determine if the user is authorized to make this request.
+     */
+    public function authorize(): bool
+    {
+        return true;
+    }
+
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array<string, \Illuminate\Contracts\Validation\ValidationRule|array<mixed>|string>
+     */
+    public function rules(): array
+    {
+        return [
+            'name' => 'required|string',
+            'description' => 'string',
+            'price' => 'required|numeric',
+        ];
+    }
+}
