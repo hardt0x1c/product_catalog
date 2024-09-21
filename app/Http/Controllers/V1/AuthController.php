ec85c912b3a5eb4f0b19db24717dfae1888bc79d
Constantine <beloglazov.constantin@gmail.com>
2024-09-21 23:21:24 +0300

    chore: installed sanctum

---
+++ app/Http/Controllers/V1/AuthController.php
@@ -0,0 +1,104 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Http\Controllers\V1;
+
+use App\Http\Controllers\Controller;
+use App\Models\User;
+use Illuminate\Http\JsonResponse;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Hash;
+use Illuminate\Support\Facades\Validator;
+
+final class AuthController extends Controller
+{
+    public function register(Request $request): JsonResponse
+    {
+        try {
+            $validateUser = Validator::make($request->all(),
+                [
+                    'name' => 'required',
+                    'email' => 'required|email|unique:users,email',
+                    'password' => 'required',
+                ]);
+
+            if ($validateUser->fails()) {
+                return response()->json([
+                    'status' => false,
+                    'message' => 'validation error',
+                    'errors' => $validateUser->errors(),
+                ], 401);
+            }
+
+            $user = User::create([
+                'name' => $request->name,
+                'email' => $request->email,
+                'password' => Hash::make($request->password),
+            ]);
+
+            return response()->json([
+                'status' => true,
+                'message' => 'User Created Successfully',
+                'token' => $user->createToken('API TOKEN')->plainTextToken,
+            ], 200);
+
+        } catch (\Throwable $th) {
+            return response()->json([
+                'status' => false,
+                'message' => $th->getMessage(),
+            ], 500);
+        }
+    }
+
+    /**
+     * Login The User
+     */
+    public function login(Request $request): JsonResponse
+    {
+        try {
+            $validateUser = Validator::make($request->all(),
+                [
+                    'email' => 'required|email',
+                    'password' => 'required',
+                ]);
+
+            if ($validateUser->fails()) {
+                return response()->json([
+                    'status' => false,
+                    'message' => 'validation error',
+                    'errors' => $validateUser->errors(),
+                ], 401);
+            }
+
+            if (! Auth::attempt($request->only(['email', 'password']))) {
+                return response()->json([
+                    'status' => false,
+                    'message' => 'Email & Password does not match with our record.',
+                ], 401);
+            }
+
+            $user = User::where('email', $request->email)->first();
+
+            if (! $user) {
+                return response()->json([
+                    'status' => false,
+                    'message' => 'User not found.',
+                ], 404);
+            }
+
+            return response()->json([
+                'status' => true,
+                'message' => 'User Logged In Successfully',
+                'token' => $user->createToken('API TOKEN')->plainTextToken,
+            ], 200);
+
+        } catch (\Throwable $th) {
+            return response()->json([
+                'status' => false,
+                'message' => $th->getMessage(),
+            ], 500);
+        }
+    }
+}
--- routes/v1.php
+++ routes/v1.php
@@ -5,6 +5,10 @@ declare(strict_types=1);
 use App\Http\Controllers\V1;
 use Illuminate\Support\Facades\Route;
 
-Route::apiResource('/categories', V1\CategoryController::class);
-Route::get('/products/search', [V1\ProductController::class, 'search'])->name('products.search');
-Route::apiResource('/products', V1\ProductController::class);
+Route::apiResource('/categories', V1\CategoryController::class)->middleware('auth:sanctum');
+Route::get('/products/search', [V1\ProductController::class, 'search'])->name('products.search')->middleware('auth:sanctum');
+Route::apiResource('/products', V1\ProductController::class)->middleware('auth:sanctum');
+Route::post('register', [V1\AuthController::class, 'register']);
+Route::post('login', [V1\AuthController::class, 'login'])->name('login');
+Route::post('logout', [V1\AuthController::class, 'logout'])
+    ->middleware('auth:sanctum');
