9d841bff21085b220b852a62cbc3db3c33c8068f
Constantine <beloglazov.constantin@gmail.com>
2024-09-22 14:02:51 +0300

    feat: added annotations for swagger

--- app/Models/Category.php
+++ app/Models/Category.php
@@ -9,6 +9,16 @@ use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\HasMany;
 
+/**
+ * @OA\Schema(
+ *     schema="Category",
+ *     type="object",
+ *     title="Категория",
+ *
+ *     @OA\Property(property="id", type="integer", description="ID категории"),
+ *     @OA\Property(property="name", type="string", description="Название категории"),
+ * )
+ */
 final class Category extends Model
 {
     /** @use HasFactory<CategoryFactory> */
--- app/Models/Product.php
+++ app/Models/Product.php
@@ -9,6 +9,39 @@ use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\BelongsTo;
 
+/**
+ * @OA\Schema(
+ *     schema="Product",
+ *     required={"name", "price"},
+ *
+ *     @OA\Property(
+ *         property="id",
+ *         type="integer",
+ *         description="Product ID"
+ *     ),
+ *     @OA\Property(
+ *         property="name",
+ *         type="string",
+ *         description="Product name"
+ *     ),
+ *     @OA\Property(
+ *         property="description",
+ *         type="string",
+ *         description="Product description"
+ *     ),
+ *     @OA\Property(
+ *         property="price",
+ *         type="number",
+ *         format="integer",
+ *         description="Product price"
+ *     ),
+ *     @OA\Property(
+ *         property="category",
+ *         ref="#/components/schemas/Category",
+ *         description="Category that the product belongs to"
+ *     ),
+ * )
+ */
 final class Product extends Model
 {
     /** @use HasFactory<ProductFactory> */
